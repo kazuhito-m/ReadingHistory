62e0dcb30c3f01aea9dd820caa7a28fb0416e5fd
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-04-17 02:38:03 +0900

    実装。PostgresのMigrationができるところまで。 Migration内容は未実装。

---
+++ ReadingHistoryMigration/Program.cs
@@ -0,0 +1,42 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ReadingHistoryMigration
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=localhost;Database=reading_history;Username=reading_history;Password=reading_history";
+
+            var upgrader =
+                DeployChanges.To
+                    .PostgresqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ ReadingHistoryMigration/Scripts/V1_1_1__Create_table.sql
@@ -0,0 +1,12 @@
+﻿-- 自身アプリケーションデータ。
+
+
+-- テーブル作成。
+
+-- 漫画。
+CREATE TABLE Comics
+(
+    ComicId INT NOT NULL,
+    PreSubTitle VARCHAR (255) NULL,
+    CONSTRAINT PK_Comics PRIMARY KEY (ComicId)
+);
